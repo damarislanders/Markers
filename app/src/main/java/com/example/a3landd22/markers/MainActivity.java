71014feabc378c186af8dc2d6af602cf7a4b33b3
damarislanders <3landd22@solent.ac.uk>
2018-04-19 10:56:01 +0100

    - Amended the new OverlayItem() to include the components that we need to utilise - Converted the GeoPoint components to double - Added ItemizedIconOverlay ... markerGestureListener to MapView mv - New markerGestureListener with two methods: onItemLongPress() and onItemSingleTapUp() - corrected file path to poi.txt --> Environment.getExternalStorageDirectory().getAbsolutePath()+"/filepath" - included component[2] to include type - if statement to define whether the marker is a pub or a restaurant

--- app/src/main/java/com/example/a3landd22/markers/MainActivity.java
+++ app/src/main/java/com/example/a3landd22/markers/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.a3landd22.markers;
 
+import android.os.Environment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -22,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     MapView mv;
     ItemizedIconOverlay<OverlayItem> items;
-
+    ItemizedIconOverlay.OnItemGestureListener<OverlayItem> markerGestureListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,18 +37,42 @@ public class MainActivity extends AppCompatActivity {
         mv = (MapView) findViewById(R.id.map1);
 
         mv.getController().setZoom(14);
-        mv.getController().setCenter(new GeoPoint(53.4113, -2.1601));
+        //mv.getController().setCenter(new GeoPoint(53.4113, -2.1601));
+        mv.getController().setCenter(new GeoPoint(50.911, -1.40448));
 
-        items = new ItemizedIconOverlay<OverlayItem>(this, new ArrayList<OverlayItem>(), null);
+        markerGestureListener = new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>()
+        {
+            public boolean onItemLongPress(int i, OverlayItem item)
+            {
+                Toast.makeText(MainActivity.this, item.getSnippet(), Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+            public boolean onItemSingleTapUp(int i, OverlayItem item)
+            {
+                Toast.makeText(MainActivity.this, item.getSnippet(), Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        };
+
+        items = new ItemizedIconOverlay<OverlayItem>(this, new ArrayList<OverlayItem>(), markerGestureListener);
 
         try
         {
-            BufferedReader reader = new BufferedReader(new FileReader("poi.txt"));
+            BufferedReader reader = new BufferedReader(new FileReader(Environment.getExternalStorageDirectory().getAbsolutePath()+"/poi.txt"));
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] components = line.split(",");
                 if (components.length == 5) {
-                    OverlayItem poi = new OverlayItem(components[0], components[1], components[2], components[3], components[4]);
+                    OverlayItem poi = new OverlayItem(components[0], components[1], components[2], new GeoPoint( Double.parseDouble(components[4]), Double.parseDouble(components[3]) ) );
+                    if ( components[1].equals("pub") )
+                    {
+                        poi.setMarker(getResources().getDrawable(R.drawable.pub));
+                    }
+                    else
+                    {
+                        poi.setMarker(getResources().getDrawable(R.drawable.restaurant));
+                    }
                     items.addItem(poi);
                 }
             }
@@ -58,11 +83,13 @@ public class MainActivity extends AppCompatActivity {
                     setMessage("ERROR: " + e).show();
         }
 
+        /*
         OverlayItem myHouse = new OverlayItem("My House", "My house by the Goyt river", new GeoPoint(53.4181, -2.1406));
         OverlayItem pearMill = new OverlayItem("Pear Mill", "A converted mill, now used for activities and shops", new GeoPoint(53.4139, -2.1340));
 
         items.addItem(myHouse);
         items.addItem(pearMill);
+        */
 
         mv.getOverlays().add(items);
     }
